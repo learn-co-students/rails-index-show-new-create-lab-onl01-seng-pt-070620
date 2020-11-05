305bdfcbb9c1e25b48cbc9863e871f8cf8e7fa72
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-11-04 21:11:46 -0800

    Done.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,5 @@
 class Coupon < ActiveRecord::Base
-    def show_coupon
-        self.coupon_code + "." + self.store
-    end 
+    # def show_coupon
+    #     self.coupon_code + "." + self.store
+    # end 
 end 
