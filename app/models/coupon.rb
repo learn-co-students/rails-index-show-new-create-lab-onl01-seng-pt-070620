c09f7c92280e23f874208102e513e1f0a946ec0f
keesha <kharg14@gmail.com>
2020-11-20 21:41:33 -0500

    Done.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,7 @@
-class Coupon < ApplicationRecord
+class Coupon < ActiveRecord::Base
+
+    def to_s
+        self.coupon_code + " " + self.store
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  resources :coupons, only: [:index, :show, :new, :create]
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :coupons, only: [:index, :new, :create]
+  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
 end
